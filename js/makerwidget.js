f899eee77cae26093b46f35da8764a18eb14e5ec
pateskinasy <pateskinasy@users.noreply.github.com>
2019-07-02 16:49:01 +0100

    adding makerwidget js

---
+++ js/makerwidget.js
@@ -0,0 +1,12 @@
+(function(d, h, m){
+        var js, fjs = d.getElementsByTagName(h)[0];
+        if (d.getElementById(m)){return;}
+        js = d.createElement(h); js.id = m;
+        js.onload = function(){
+            window.makerWidgetComInit({
+            position: "left",          
+            widget: "vw0qnrt2npfrjgys-sopevuu7m5fk8dp7-lbq2thiby0qeyagf"                
+        })};
+        js.src = "https://makerwidget.com/js/embed.js";
+        fjs.parentNode.insertBefore(js, fjs)
+    }(document, "script", "dhm"))
